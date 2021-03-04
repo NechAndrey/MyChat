1248428c19ed880d83b0c43d5f321ea6d32f51f6
NechAndrey <72864146+NechAndrey@users.noreply.github.com>
2021-03-04 20:42:34 +0300

    Add files via upload

---
+++ MyChat/main.java
@@ -0,0 +1,10 @@
+package MyChat;
+
+import java.io.IOException;
+
+public class main {
+    public static void main(String[] args) throws IOException {
+       EchoClient echoClient = new EchoClient();
+       echoClient.EchoClient();
+    }
+}
